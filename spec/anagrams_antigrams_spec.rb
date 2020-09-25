05cd7d5cdc821c82f19331012d5be3c974d37307
Brian Harvey <brian.harv3y@gmail.com>
2020-09-25 08:31:09 -0700

    write spec one

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -1,2 +1,15 @@
 require('rspec')
 require('anagrams_antigrams')
+
+
+Describe('Checker') do
+  
+  describe('Checker#is_anagram') do
+    it('checks to see if user input is an anagram or not') do
+      new_checker = Checker.new("door", "odor")
+      expect(new_checker.is_anagram?()).to(eq("true"))
+    end
+  end
+
+  
+end
