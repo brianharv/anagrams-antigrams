fd6f37103d1fb8371939b936bb377c530fb36605
Brian Harvey <brian.harv3y@gmail.com>
2020-10-06 19:50:07 -0700

    begin dramatic refactor

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -11,57 +11,12 @@ class Checker
   end
 
   def is_word_anagram?
-    array_one = []
-    array_two = []
-    word_array_one.each do |word|
-      array_one.push(word.split('').sort.join)
-    end
-    array_one
-
-    word_array_two.each do |word|
-      array_two.push(word.split('').sort.join)
-    end
-    array_two
-
-    if array_one[0] == array_two[0] || array_one[1] == array_two[1] || array_one[2] == array_two[2] || array_one[3] == array_two[3]
-      puts "Yes! \n'#{input_one}' and '#{input_two}' contain anagrams!"
+    if 
       return true
     else
-      puts "I'm sorry, '#{input_one}' and '#{input_two}' do not provide anagrams"
-      return false
-    end 
-  end
-  
-  def is_antigram?
-    array_one = []
-    array_two = []
-    index = 0
-    word_array_one.each do |word|
-      array_one.push(word.split('').sort.join)
-    end
-    array_one
-
-    word_array_two.each do |word|
-      array_two.push(word.split('').sort.join)
-    end
-    array_two
-
-    antigram_array = []
-    while index < array_two.length
-      array_one.each do |word|
-        if /["#{array_one[index]}"]/.match(array_two[index]) 
-          puts "Not Antigram"
-          antigram_array.push(false)
-        else
-          puts "ANTIGRAM"
-          antigram_array.push(true)
-        end
-      end  
-    index += 1
-    return antigram_array
-    end        
-  end
-
+      false
+    end    
+  end      
 
   def is_word?
     array_one = []
